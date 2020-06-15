7aad21c8cfdeb45fdf319e6469ea66f66c863ec2
suprabha <suprabha.suprabha@go-jek.com>
2020-06-15 21:45:17 +0530

    feat(for-label): added for and for label twitter questions

---
+++ twitter/code-snippet/15-06-2020/for-solution.js
@@ -0,0 +1,11 @@
+var num = 0;
+for(var i = 0; i < 10; i++) {
+    for(var j = 0; j < 10; j++) {
+        if(i == 5 && j == 5) {
+            break;
+        }
+        num++;
+    }
+}
+
+// output: 94
